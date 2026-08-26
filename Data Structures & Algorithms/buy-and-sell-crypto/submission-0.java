12bbf3531cb14f0b15f72acda80a0d99c371dde0
Tarun Sharma <68682752+Tarun-web@users.noreply.github.com>
2026-08-26 12:40:20 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int sell = 0;
+        int mxProfit = 0;
+
+        for(int i=1; i<prices.length; i++){
+            mxProfit = Math.max(mxProfit, prices[i] - buy);
+            buy = Math.min(buy, prices[i]);
+        }
+        return mxProfit;
+    }
+}
